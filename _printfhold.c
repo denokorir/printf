40166348da6486692ac1c6380f47c2df926204d2
Irene Makonnen <irenemakonnen@gmail.com>
2023-03-29 04:04:05 +0300

    Revert "Declaration of _putchar"
    This reverts commit 338d50f86392b10f0c5610c1dbb4b208391ace54.

--- _printfhold.c
+++ _printfhold.c
@@ -1,20 +1,19 @@
-#include <stdarg.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
-
+#include "_putchar.c"
+#include <stdarg.h>
 /**
- * _printf - prints a formated string to stdout
- * @format: format of the string
- * Return: number of characters printed
+ * _printf - prints format to stdout
+ * @format: format to be printed
+ * Return: chars printed
  */
-
 int _printf(const char *format, ...)
 {
         va_list args;
         int chars_printed = 0;
+        int div = 1;
 
         va_start(args, format);
+
         while (*format != '\0') {
                 if (*format == '%') {
                         format++;
@@ -24,21 +23,40 @@ int _printf(const char *format, ...)
                         }
                         else if (*format == 's') {
                                 const char *s = va_arg(args, const char *);
+
                                 while (*s != '\0') {
                                         _putchar(*s);
                                         s++;
                                         chars_printed++;
                                 }
                         }
+                        else if (*format == 'd' || *format == 'i') {
+                                int number = va_arg(args, int);
+                                if (number < 0) {
+                                        _putchar('-');
+                                        number = -number;
+                                        chars_printed++;
+                                }
+                                while (number / div > 9) {
+                                        div *= 10;
+ 					  }
+                                while (div > 0) {
+                                        _putchar ('0' + number / div);
+                                        number %= div;
+                                        div /= 10;
+                                        chars_printed++;
+                                }
+                        }
                         else if (*format == '%') {
                                 _putchar('%');
                                 chars_printed++;
-                        } else {
-                                _putchar('&');
-                                _putchar(*format);
-                                chars_printed += 2;
                         }
+                        else {
+                        _putchar('%');
+                        _putchar(*format);
+                        chars_printed +=2;
                 }
+        }
                 else {
                         _putchar(*format);
                         chars_printed++;
@@ -48,4 +66,3 @@ int _printf(const char *format, ...)
         va_end(args);
         return (chars_printed);
 }
-
